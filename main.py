b3347ca89fb1e2d0c4f7301c3fc8f74268be672c
ChavinloSocialRise <carlos@socialrisestudio.org>
2024-09-03 12:43:17 -0500

    Bloat

--- main.py
+++ main.py
@@ -6,23 +6,6 @@ from ultralytics import YOLO
 
 image_path = "test.jpg"
 
-def line_intersection(line1, line2):
-    xdiff = (line1[0][0] - line1[1][0], line2[0][0] - line2[1][0])
-    ydiff = (line1[0][1] - line1[1][1], line2[0][1] - line2[1][1])
-
-    def det(a, b):
-        return a[0] * b[1] - a[1] * b[0]
-
-    div = det(xdiff, ydiff)
-    if div == 0:
-       raise Exception('lines do not intersect')
-
-    d = (det(*line1), det(*line2))
-    x = det(d, xdiff) / div
-    y = det(d, ydiff) / div
-    return x, y
-
-
 class FaceRandomizerPipeline:
     def __init__(self):
         print("Loading YOLO model...")
